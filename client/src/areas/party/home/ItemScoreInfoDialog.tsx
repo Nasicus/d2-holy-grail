3b9524d8621d8f917e860e4e0e9ad0c71d20293f
mpherman <max.pherman@gmail.com>
2019-07-05 11:55:25 -0400

    Added more info about ItemScores
    - Added some more info about ItemScores to the party main page
    - Maybe a good idea to just list all the scores? Decided to just list a few examples for now.
    - And started using Hooks :smile:

---
+++ client/src/areas/party/home/ItemScoreInfoDialog.tsx
@@ -0,0 +1,118 @@
+import * as React from "react";
+import styled from "styled-components";
+import { Typography } from "@material-ui/core";
+import { FC } from "react";
+import Dialog from "@material-ui/core/Dialog/Dialog";
+import DialogContent from "@material-ui/core/DialogContent/DialogContent";
+import Icon, { IconProps } from "@material-ui/core/Icon/Icon";
+import Table, { TableProps } from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell, { TableCellProps } from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+export interface IInfoProps {
+  onDialogClosed: () => any;
+}
+
+export const ItemScoreInfoDialog: FC<IInfoProps> = ({ onDialogClosed }) => {
+  return (
+    <Dialog open={true} onClose={onDialogClosed}>
+      <DialogContent>
+        <div>
+          <CloseIcon onClick={onDialogClosed}>close</CloseIcon>
+          <Typography variant="h4">
+            Introducing ItemScore! <br />
+            <br />
+          </Typography>
+          <Typography variant="body1">
+            A unique or set item's "Item Score" is based on it's relative rarity
+            to that ultimate Holy Grail in Diablo II: Tyrael's Might. <br />
+            A list of example ItemScores can be found below: <br />
+          </Typography>
+          <StyledTable>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Item</StyledTableCell>
+                <StyledTableCell>Score</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <StyledTableCell>Nokozan Relic</StyledTableCell>
+                <StyledTableCell>1</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>Nagelring</StyledTableCell>
+                <StyledTableCell>1</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>Venom Ward</StyledTableCell>
+                <StyledTableCell>1</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>...</StyledTableCell>
+                <StyledTableCell>...</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>Mara's Kaleidoscope</StyledTableCell>
+                <StyledTableCell>30</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>Windforce</StyledTableCell>
+                <StyledTableCell>110</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>Crown of Ages</StyledTableCell>
+                <StyledTableCell>130</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>Astreon's Iron Ward</StyledTableCell>
+                <StyledTableCell>335</StyledTableCell>
+              </TableRow>
+              <TableRow>
+                <StyledTableCell>Tyrael's Might</StyledTableCell>
+                <StyledTableCell>1155</StyledTableCell>
+              </TableRow>
+            </TableBody>
+          </StyledTable>
+          <BottomText>
+            The values have been slightly increased across the board so that
+            each item has a value of 1 or more. <br />
+            Some items were so common compared to Tyrael's that they would round
+            down to 0.
+          </BottomText>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const CloseIcon: React.ComponentType<IconProps> = styled(Icon)`
+  && {
+    position: absolute;
+    top: ${p => p.theme.spacing(1)}px;
+    right: ${p => p.theme.spacing(1)}px;
+    cursor: pointer;
+  }
+`;
+
+const StyledTableCell: React.ComponentType<TableCellProps> = styled(TableCell)`
+  && {
+    padding: 14px 40px 14px 24px;
+  }
+`;
+
+const StyledTable: React.ComponentType<TableProps> = styled(Table)`
+  && {
+    max-width: 800px;
+    text-align: center;
+  }
+`;
+
+const BottomText = styled(Typography)`
+  && {
+    margin-top: 10px;
+    font-size: 1em;
+  }
+`;
--- client/src/areas/party/home/PartyExplanation.tsx
+++ client/src/areas/party/home/PartyExplanation.tsx
@@ -1,32 +1,47 @@
 import * as React from "react";
 import styled from "styled-components";
 import { Typography } from "@material-ui/core";
+import { ButtonProps } from "@material-ui/core/Button";
+import { Button } from "@material-ui/core";
+import { FC, useState } from "react";
+import { ItemScoreInfoDialog } from "./ItemScoreInfoDialog";
 
-export class PartyExplanation extends React.Component {
-  public constructor(props: {}) {
-    super(props);
-    this.state = {};
-  }
-
-  public render() {
-    return (
-      <RootContainer>
-        <Typography variant="h4">
-          Introducing Holy Grail Parties! <br />
-        </Typography>
-        <Typography variant="body1">
-          Ever wanted to compare your grail progress to friends or other members
-          of a community? <br />
-          Simply create a Holy Grail Party above and invite your friends! <br />
-          The party will automatically keep track of each member's grail
-          progress as they enter new items. <br />
-          Compare your progress with a simple table of all users' grails in your
-          party.
-        </Typography>
-      </RootContainer>
-    );
-  }
-}
+export const PartyExplanation: FC = () => {
+  const [renderItemScoreInfo, setRenderItemScoreInfo] = useState(false);
+  return (
+    <RootContainer>
+      {renderItemScoreInfo && (
+        <ItemScoreInfoDialog
+          onDialogClosed={() => {
+            setRenderItemScoreInfo(false);
+          }}
+        />
+      )}
+      <Typography variant="h4">
+        Introducing Holy Grail Parties! <br />
+      </Typography>
+      <Typography variant="body1">
+        Ever wanted to compare your grail progress to friends or other members
+        of a community? <br />
+        Simply create a Holy Grail Party above and invite your friends! <br />
+        The party will automatically keep track of each member's grail progress
+        as they enter new items. <br />
+        Compare your progress with a simple table of all users' grails in your
+        party. <br />
+        <br />
+        Also introducing ItemScore! A way to compare grail progress based on the
+        rarity of items collected. <br />
+        <span>
+          You can
+          <Button onClick={() => setRenderItemScoreInfo(true)}>
+            Click Here
+          </Button>
+          to see more information about it.
+        </span>
+      </Typography>
+    </RootContainer>
+  );
+};
 
 const RootContainer = styled.div`
   width: 700px;
