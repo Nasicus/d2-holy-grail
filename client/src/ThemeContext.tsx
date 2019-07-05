b9f5ceb4c0d544c02a706fe70501e53d889908dd
Nasicus <pz@tenhouse.ch>
2019-07-06 00:13:17 +0200

    Improve setting the theme & the title for the app

---
+++ client/src/ThemeContext.tsx
@@ -0,0 +1,33 @@
+import { createContext } from "react";
+import { Theme, createMuiTheme } from "@material-ui/core";
+import { purple, green, brown, grey } from "@material-ui/core/colors";
+
+export const ThemeContext = createContext<{
+  theme: Theme;
+  title: string;
+  setTheme: (theme: Theme, title: string) => any;
+}>(null);
+
+export const normalTheme: Theme = createMuiTheme({
+  typography: {},
+  palette: {
+    primary: purple,
+    secondary: green
+  }
+});
+
+export const ethTheme: Theme = createMuiTheme({
+  typography: {},
+  palette: {
+    primary: brown,
+    secondary: grey
+  }
+});
+
+export const runewordTheme: Theme = createMuiTheme({
+  typography: {},
+  palette: {
+    primary: grey,
+    secondary: brown
+  }
+});
